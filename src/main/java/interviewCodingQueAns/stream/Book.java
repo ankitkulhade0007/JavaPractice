abfef46e7b7cfe0173bec7eea72051be87ade0e9
ankitkulhade0007 <ankitkulhade123@gmail.com>
2024-10-20 02:29:22 +0530

    Java Stream Question And Grouping  Example

---
+++ src/main/java/interviewCodingQueAns/stream/Book.java
@@ -0,0 +1,50 @@
+package interviewCodingQueAns.stream;
+
+public class Book {
+
+    String title;
+    String genre;
+    String author;
+
+    public Book(String title, String genre, String author) {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+    }
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
