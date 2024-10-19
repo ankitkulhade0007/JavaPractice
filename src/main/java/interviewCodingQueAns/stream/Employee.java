a9e64321e90fa4126484bc210a9aed2bcca6fe3e
ankitkulhade0007 <ankitkulhade123@gmail.com>
2024-10-20 02:33:02 +0530

    Java Stream Question And Grouping  Example

--- src/main/java/interviewCodingQueAns/stream/Employee.java
+++ src/main/java/interviewCodingQueAns/stream/Employee.java
@@ -27,22 +27,13 @@ public class Employee {
     public String getDepartment() {
         return department;
     }
-    public void setDepartment(String department) {
-        this.department = department;
-    }
+
     public double getSalary() {
         return salary;
-    }
-    public void setSalary(double salary) {
-        this.salary = salary;
     }
 
     public String getGender() {
         return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
     }
 
     @Override
